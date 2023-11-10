7a5f132f9016eef8937a5dd731448441bd406035
Albin Tallkvist <albin.tallkvist@elev.ga.ntig.se>
2023-11-10 12:41:29 +0100

    vrf e karaktär onsynlig ;( ;( ;(

--- VinterprojektetProg2/Program.cs
+++ VinterprojektetProg2/Program.cs
@@ -5,15 +5,16 @@
 
 // Raylib inställningarna
 using Raylib_cs;
+
 Raylib.InitWindow(1280, 800, "Untitled");
 Raylib.SetTargetFPS(60);
-string currentScene = "StartScreen";  // Scen som visas när game start
+string currentScene = "StartScreen";  
 float WalkSpeed = 2f;
 
 
 // ladda in texturer här - hitta bra textur för playable character på freda
-Texture2D PlayerModel = Raylib.LoadTexture("HarIngenÄn.png");
-Rectangle player = new Rectangle(415, 60, 80, 80);
+Texture2D PlayerModel = Raylib.LoadTexture("PlayableChar.png");
+Rectangle player = new Rectangle(415, 60, PlayerModel.Width, PlayerModel.Height);
 Color backgroundcolor = new Color(255, 255, 255, 255);
 
 
@@ -23,6 +24,52 @@ Color backgroundcolor = new Color(255, 255, 255, 255);
 // raylib grundläggande kod, bytta skit å controls osv
 while (Raylib.WindowShouldClose() == false)
 {
+
+    if (Raylib.IsKeyDown(KeyboardKey.KEY_D))
+    {
+      player.X += WalkSpeed;
+
+      if (player.X > Raylib.GetScreenWidth() - player.Width) 
+        {
+          player.X = Raylib.GetScreenWidth() - player.Width; 
+        }
+    }
+
+
+    if (Raylib.IsKeyDown(KeyboardKey.KEY_A))
+    {
+      player.X -= WalkSpeed;
+
+      if (player.X < 0) 
+        {
+            player.X = 0;
+        }
+    }
+
+
+    if (Raylib.IsKeyDown(KeyboardKey.KEY_S))
+    {
+      player.Y += WalkSpeed;
+
+      if (player.Y > Raylib.GetScreenHeight() - player.Height) 
+        {
+            player.Y = Raylib.GetScreenHeight() - player.Height; 
+        }
+    }
+
+
+    if (Raylib.IsKeyDown(KeyboardKey.KEY_W))
+  {
+    player.Y -= WalkSpeed;
+
+    if (player.Y < 0) 
+        {
+            player.Y = 0; 
+        }
+  }
+
+
+
 
 
 
@@ -36,30 +83,36 @@ while (Raylib.WindowShouldClose() == false)
 
 
 
+
+
+
 // rita
 
     Raylib.BeginDrawing();
     Raylib.ClearBackground(backgroundcolor);
 
-    if (currentScene == "StartScreen")
+
+    if (currentScene == "StartRoom")
+    {
+        Raylib.DrawText("Test", 280, 300, 50, Color.BLACK);
+        Raylib.DrawTexture(PlayerModel, (int) player.X, (int) player.Y, backgroundcolor);
+
+
+    }
+
+      else if (currentScene == "StartScreen")
     {
         Raylib.DrawText("Welcome", 280, 380, 50, Color.BLACK);
         Raylib.DrawText("\nENTER to begin", 515, 420, 32, Color.BLACK);
 
     } 
 
-    else if (currentScene == "StartRoom")
-    {
-        Raylib.DrawText("Test", 280, 300, 50, Color.BLACK);
-    }
-
-
-
-
     Raylib.EndDrawing();
-
-
 }
+
+
+
+
 
 // för senare
 // bool = kläder etc
