7e64c1e483340070c52675a7ec9f289b90acb451
Albin Tallkvist <albin.tallkvist@elev.ga.ntig.se>
2023-12-06 23:56:50 +0100

    fixade teleports så de onsynliga inet fanns kvar

--- VinterprojektetProg2/Program.cs
+++ VinterprojektetProg2/Program.cs
@@ -9,14 +9,10 @@ Raylib.InitWindow(1280, 800, "Hurry");
 Raylib.SetTargetFPS(60);
 string currentScene = "StartScreen";
 Texture2D playerModel = Raylib.LoadTexture("TheBoy.png");
-Character character = new Character(playerModel); 
+Character character = new Character(playerModel);
 Color backgroundcolor = new Color(255, 255, 255, 255);
 Texture2D backgroundImage = Raylib.LoadTexture("Backgroundd.png");
 Texture2D doorImage = Raylib.LoadTexture("doorone.png");
-Rectangle sceneChangeBedroom = new Rectangle(1180, 320, doorImage.Width, doorImage.Height);
-Rectangle sceneChangeBathroom = new Rectangle(0, 320, doorImage.Width, doorImage.Height);
-Rectangle sceneChangeLivingroom = new Rectangle(0, 320, doorImage.Width, doorImage.Height);
-
 
 void ResetCharacterPosition()
 {
@@ -24,50 +20,26 @@ void ResetCharacterPosition()
     character.player.Y = Raylib.GetScreenHeight() / 2 - character.PlayerModel.Height / 2;
 }
 
-// --------------------------------------------------------------------------------
-
 while (Raylib.WindowShouldClose() == false)
 {
-  if (currentScene == "StartScreen")
-  {
-    if (Raylib.IsKeyDown(KeyboardKey.KEY_ENTER))
-    {
-      currentScene = "StartRoom";
-    }
-  }
-
-
-  if (Raylib.CheckCollisionRecs(character.player, sceneChangeBedroom))
-  {
-    currentScene = "bedroomscene";
-    ResetCharacterPosition();
-
-    
-  }
-
-  if (Raylib.CheckCollisionRecs(character.player, sceneChangeBathroom))
-  {
-    currentScene = "bathroomscene";
-    ResetCharacterPosition();
-
-  }
-
-  if (Raylib.CheckCollisionRecs(character.player, sceneChangeLivingroom))
-  {
-    currentScene = "livingroom";
-    ResetCharacterPosition();
-
-  }
-
-
-
-
     Raylib.BeginDrawing();
     Raylib.ClearBackground(backgroundcolor);
 
-    if (currentScene == "StartRoom")
+    if (currentScene == "StartScreen")
     {
-       character.Update();
+        if (Raylib.IsKeyDown(KeyboardKey.KEY_ENTER))
+        {
+            currentScene = "StartRoom";
+        }
+
+        Raylib.DrawText("Welcome", 560, 420, 40, Color.BLACK);
+        Raylib.DrawText("\nENTER to begin", 515, 420, 32, Color.BLACK);
+    }
+    else if (currentScene == "StartRoom")
+    {
+        character.Update();
+        Rectangle sceneChangeBedroom = new Rectangle(1180, 320, doorImage.Width, doorImage.Height);
+        Rectangle sceneChangeBathroom = new Rectangle(0, 320, doorImage.Width, doorImage.Height);
 
         Raylib.DrawTexture(backgroundImage, 0, 0, Color.WHITE);
         Raylib.DrawTexture(character.PlayerModel, (int)character.player.X, (int)character.player.Y, backgroundcolor);
@@ -76,32 +48,43 @@ while (Raylib.WindowShouldClose() == false)
         Raylib.DrawRectangleRec(sceneChangeBathroom, Color.BROWN);
         Raylib.DrawTexture(doorImage, (int)sceneChangeBedroom.X, (int)sceneChangeBedroom.Y, Color.WHITE);
         Raylib.DrawTexture(doorImage, (int)sceneChangeBathroom.X, (int)sceneChangeBathroom.Y, Color.WHITE);
+
+        if (Raylib.CheckCollisionRecs(character.player, sceneChangeBedroom))
+        {
+            currentScene = "bedroomscene";
+            ResetCharacterPosition();
+        }
+
+        else if (Raylib.CheckCollisionRecs(character.player, sceneChangeBathroom))
+        {
+            currentScene = "bathroomscene"; 
+            ResetCharacterPosition();
+        }
+
+
     }
-
-      else if (currentScene == "StartScreen")
-    {
-      
-        Raylib.DrawText("Welcome", 560, 420, 40, Color.BLACK);
-        Raylib.DrawText("\nENTER to begin", 515, 420, 32, Color.BLACK);
-
-    } 
-
     else if (currentScene == "bedroomscene")
     {
         character.Update();
+        Rectangle sceneChangeLivingroom = new Rectangle(0, 320, doorImage.Width, doorImage.Height);
 
         Raylib.DrawTexture(backgroundImage, 0, 0, Color.WHITE);
         Raylib.DrawText("Bedroom", 515, 420, 32, Color.BLACK);
         Raylib.DrawTexture(character.PlayerModel, (int)character.player.X, (int)character.player.Y, backgroundcolor);
         Raylib.DrawRectangleRec(sceneChangeLivingroom, Color.BROWN);
         Raylib.DrawTexture(doorImage, (int)sceneChangeLivingroom.X, (int)sceneChangeLivingroom.Y, Color.WHITE);
-        
 
+        if (Raylib.CheckCollisionRecs(character.player, sceneChangeLivingroom))
+        {
+            currentScene = "StartRoom";
+            ResetCharacterPosition();
+        }
     }
-
-     else if (currentScene == "bathroomscene")
+    else if (currentScene == "bathroomscene")
     {
         character.Update();
+        Rectangle sceneChangeLivingroom = new Rectangle(1180, 320, doorImage.Width, doorImage.Height);
+
 
         Raylib.DrawTexture(backgroundImage, 0, 0, Color.WHITE);
         Raylib.DrawText("Bathroom", 515, 420, 32, Color.BLACK);
@@ -109,10 +92,18 @@ while (Raylib.WindowShouldClose() == false)
         Raylib.DrawRectangleRec(sceneChangeLivingroom, Color.BROWN);
         Raylib.DrawTexture(doorImage, (int)sceneChangeLivingroom.X, (int)sceneChangeLivingroom.Y, Color.WHITE);
 
+        if (Raylib.CheckCollisionRecs(character.player, sceneChangeLivingroom))
+        {
+            currentScene = "StartRoom";
+            ResetCharacterPosition();
+        }
     }
+     
+    
 
     Raylib.EndDrawing();
 }
+
 
 
 
